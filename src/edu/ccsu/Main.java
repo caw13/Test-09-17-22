b29b7a7eb8193e3a59ba8d42c4ad11e4698dc737
Chad Williams <chadgit13@gmail.com>
2022-09-19 18:42:55 -0400

    Added ver 2 changes

--- src/edu/ccsu/Main.java
+++ src/edu/ccsu/Main.java
@@ -3,8 +3,9 @@ package edu.ccsu;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-
+		System.out.println("Hello world ver 2");
+		System.out.println("something else");
+		
 	}
 
 }
